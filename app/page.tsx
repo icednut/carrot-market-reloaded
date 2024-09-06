22578a4999a55915097374a773db4fef4c5ccb27
icednut <wglee21g@gmail.com>
2024-09-06 12:48:41 +0900

    exercise tailwindcss (odd, even)

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (<main className="bg-gray-100 h-screen flex items-center justify-center p-5 sm:bg-red-200 md:bg-green-200 lg:bg-cyan-200 xl:bg-orange-200 2xl:bg-purple-200">
-    <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none has-[.peer]:bg-green-100 has-[:invalid]:bg-red-100 has-[:invalid]:ring has-[:invalid]:ring-red-500 ring ring-transparent transition-shadow">
-      <input className="w-full rounded-full bg-gray-200 pl-5 h-12 ring ring-transparent invalid:focus:ring-orange-500 focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow peer" type="text" placeholder="Email Address" required />
-      <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required.</span>
-      <button className="text-white py-2 rounded-full active:scale-95 transition-transform font-medium md:px-10 peer-invalid:bg-black  peer-required:bg-green-500">Login</button>
+    <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+      {["Nico", "Me", "You", "Yourself"].map((person, index) => 
+        <div key={index} className="flex items-center gap-5 odd:bg-gray-100 even:bg-purple-100 p-2.5 rounded-xl">
+          <div className= "size-10 bg-blue-400 rounded-full" />
+          <span className="text-lg font-medium">{person}</span>
+          <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
+            <span>{index}</span>
+          </div>
+        </div>)}
     </div>
   </main>);
 }
