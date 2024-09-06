1a5c8068d0e5da95df60d1ac0460da04a4204d3b
icednut <wglee21g@gmail.com>
2024-09-06 13:04:46 +0900

    exercise tailwindcss (ping animate)

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (<main className="bg-gray-100 h-screen flex items-center justify-center p-5 sm:bg-red-200 md:bg-green-200 lg:bg-cyan-200 xl:bg-orange-200 2xl:bg-purple-200">
     <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
-      {["Nico", "Me", "You", "Yourself"].map((person, index) => 
-        <div key={index} className="flex items-center gap-5 *:animate-pulse">
+      {["Nico", "Me", "You", "Yourself", ""].map((person, index) => 
+        <div key={index} className="flex items-center gap-5">
           <div className= "size-10 bg-blue-400 rounded-full" />
-          <div className="w-40 h-4 bg-gray-400 rounded-full" />
-          <div className="w-20 h-4 bg-gray-400 rounded-full" />
-          {/* <span className="text-lg font-medium">{person}</span> */}
-          {/* <div className="size-6 animate-spin bg-red-500 text-white flex items-center justify-center rounded-full">
-            <span>☢</span>
-          </div> */}
+          <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">{person}</span>
+          <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
+            <span className="z-10">{index}</span>
+            <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
+          </div>
         </div>)}
     </div>
   </main>);
