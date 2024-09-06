f02f9f9016011367a8b18e23058281733a8ec813
icednut <wglee21g@gmail.com>
2024-09-06 21:57:36 +0900

    add login

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,17 +1,21 @@
-import Link from "next/link";
-import ChatBubbleOvalLeftEllipsisIcon from "@heroicons/react/24/solid/ChatBubbleOvalLeftEllipsisIcon";
 import FormInput from "@/components/form-input";
 import FormButton from "@/components/form-btn";
 import SocialLogin from "@/components/social-login";
 
-export default function Login() {
+export default function CreateAccount() {
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">Log in with email and password</h2>
+        <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
       <form className="flex flex-col gap-3">
+        <FormInput
+          type="text"
+          placeholder="Username"
+          required={true}
+          errors={[]}
+        />
         <FormInput
           type="email"
           placeholder="Email"
@@ -21,6 +25,12 @@ export default function Login() {
         <FormInput
           type="password"
           placeholder="Password"
+          required={true}
+          errors={[]}
+        />
+        <FormInput
+          type="password"
+          placeholder="Confirm Password"
           required={true}
           errors={[]}
         />
---
+++ components/social-login.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import ChatBubbleOvalLeftEllipsisIcon from "@heroicons/react/24/solid/ChatBubbleOvalLeftEllipsisIcon";
+
+export default function SocialLogin() {
+  return (
+    <>
+      <div className="w-full h-px bg-neutral-500" />
+      <div className="flex flex-col gap-3 ">
+        <Link className="primary-btn flex h-10 items-center justify-center gap-3" href="/github/start">
+          <span>
+            <ChatBubbleOvalLeftEllipsisIcon className="size-6" />
+          </span>
+          <span>Continue with Github</span>
+        </Link>
+        <Link className="primary-btn flex h-10 items-center justify-center gap-3" href="/sms">
+          <span>
+            <ChatBubbleOvalLeftEllipsisIcon className="size-6" />
+          </span>
+          <span>Continue with SNS</span>
+        </Link>
+      </div>    
+    </>
+  )
+}
