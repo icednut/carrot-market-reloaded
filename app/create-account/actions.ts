95402075094083e594cd753a98aec60e77d00c0a
icednut <wglee21g@gmail.com>
2024-09-18 16:18:27 +0900

    add validation with zod

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,6 +2,17 @@
 
 import { z } from "zod"
 
+const passwordRegex = new RegExp(
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/
+)
+
+function checkUsername(username: string) {
+  return !username.includes("potatoes")
+}
+
+const checkPassword = ({password, confirmPassword}: {password: string, confirmPassword: string}) => 
+  password === confirmPassword
+
 // const usernameSchema = z.string().min(3).max(10)
 const formSchema = z.object({
   username: z
@@ -10,7 +21,8 @@ const formSchema = z.object({
       required_error: "Where is my username?"
     })
     .min(3, "너무 짧습니다. Way too short!")
-    .max(20, "너무 길어요. This is too long!"),
+    .max(20, "너무 길어요. This is too long!")
+    .refine(checkUsername, "No potatoes allowed!"),
   email: z
     .string({
       invalid_type_error: "Email must be a string",
@@ -22,13 +34,17 @@ const formSchema = z.object({
       invalid_type_error: "Password must be a string",
       required_error: "Where is my password?"
     })
-    .min(10, "비밀번호가 너무 짧습니다. Way too short!"),
+    .min(10, "비밀번호가 너무 짧습니다. Way too short!")
+    .regex(passwordRegex, "A password must have lowercase, uppercase, number, and special characters"),
   confirmPassword: z
     .string({
       invalid_type_error: "Confirm Password must be a string",
       required_error: "Where is my confirm password?"
     })
     .min(10, "비밀번호가 너무 짧습니다. Way too short!")
+}).refine(checkPassword, {
+  message: "Both passwords should be the same",
+  path: ["confirmPassword"]
 })
 
 export async function createAccount(prevState: any, formData: FormData) {
@@ -41,6 +57,9 @@ export async function createAccount(prevState: any, formData: FormData) {
 
   const result = formSchema.safeParse(data)
   if (!result.success) {
-    return result.error.flatten()
+    const errorMessage = result.error.flatten()
+
+    console.log(errorMessage)
+    return errorMessage
   }
 }
